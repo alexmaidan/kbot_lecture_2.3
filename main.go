ffc4816a889faad2ce865c383be1beb8c62e0306
Oleksii Maidan <alex.maidan@gmail.com>
2024-04-02 17:53:01 +0000

    Changed repo name

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/alexmaidan/kbot/cmd"
+import "github.com/alexmaidan/kbot_lecture_2.3/cmd"
 
 func main() {
 	cmd.Execute()
